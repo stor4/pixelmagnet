967b2a09813527671c8b598f96dc06d6de84f49a
stor4 <storchak850@gmail.com>
2024-06-21 17:46:53 +0300

    testemonials section

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 console.log('components');
 
 import './components/featuredWorks.js'
+import './components/testemonials.js'
---
+++ src/js/components/testemonials.js
@@ -0,0 +1,12 @@
+const swiper1 = new Swiper('.testemonials__swiper ', {
+  direction: 'vertical',
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
